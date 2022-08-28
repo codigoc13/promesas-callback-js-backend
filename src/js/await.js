ccf8021f88d40ecee5b531cb2b0bd5f12e217211
seraleman <seraleman1204@gmail.com>
2022-08-28 18:07:07 -0500

    Manejo de errores en el await

--- src/js/await.js
+++ src/js/await.js
@@ -1,12 +1,22 @@
-import { buscarHeroe } from './promesas'
+import { buscarHeroe, buscarHeroeAsync } from './promesas'
 
 const heroesIds = ['capi', 'iron', 'spider']
 
 export const obtenerHeroesArr = async () => {
-  //   const heroesArr = []
-  //   for (const id of heroesIds) {
-  //     heroesArr.push(buscarHeroe(id))
-  //   }
-  //   return await Promise.all(heroesArr)
   return await Promise.all(heroesIds.map((id) => buscarHeroe(id)))
+}
+
+export const obtenerHeroeAwait = async (id) => {
+  try {
+    const heroe = await buscarHeroeAsync(id)
+
+    return heroe
+  } catch (err) {
+    console.log('CATCH!!!')
+    // throw err
+    return {
+      nombre: 'Sin nombre',
+      poder: 'Sin poder',
+    }
+  }
 }
