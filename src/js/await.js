486a98faab45c4ae3b6bbb1a16e1cddf42061358
seraleman <seraleman1204@gmail.com>
2022-08-28 18:21:55 -0500

    Ejemplo de del await en el for y en el if

--- src/js/await.js
+++ src/js/await.js
@@ -1,6 +1,8 @@
 import { buscarHeroe, buscarHeroeAsync } from './promesas'
 
 const heroesIds = ['capi', 'iron', 'spider']
+// const heroesPromesas = heroesIds.map((id) => buscarHeroe(id))
+const heroesPromesas = heroesIds.map(buscarHeroe)
 
 export const obtenerHeroesArr = async () => {
   return await Promise.all(heroesIds.map((id) => buscarHeroe(id)))
@@ -18,5 +20,26 @@ export const obtenerHeroeAwait = async (id) => {
       nombre: 'Sin nombre',
       poder: 'Sin poder',
     }
+  }
+}
+
+export const heroesCiclo = async () => {
+  console.time('HeroesCiclo')
+
+  // const heroes = await Promise.all(heroesPromesas)
+  // heroes.forEach((heroe) => console.log(heroe))
+
+  for await (const heroe of heroesPromesas) {
+    console.log(heroe)
+  }
+
+  console.timeEnd('HeroesCiclo')
+}
+
+export const heroeIfAwait = async (id) => {
+  if ((await buscarHeroeAsync(id)).nombre === 'Ironman') {
+    console.log('Es el mejor de todos')
+  } else {
+    console.log('Nhaaa')
   }
 }
